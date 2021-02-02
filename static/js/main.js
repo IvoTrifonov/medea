a36ebee922664eb1f45bea0bc9614ff0fcd77bbd
ivotrifonov <ivko999@abv.bg>
2021-02-02 10:06:38 +0200

    test

--- static/js/main.js
+++ static/js/main.js
@@ -6,7 +6,9 @@ AOS.init({
 
 $(document).on('ready', function () {
   setTimeout(() => {
-    $('body').css({ 'visibility': 'visible' });
+    $('body').css({
+      'visibility': 'visible'
+    });
   }, 100);
 });
 
@@ -21,31 +23,7 @@ $('#browser').text($.browser.android)
 function isFacebookApp() {
   var ua = navigator.userAgent || navigator.vendor || window.opera;
   return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
-} 
-
-$('#browser').text(`font size -> ${$('html').css('font-size')}`)
-
-
-webView.getSettings().setTextZoom(100);
-
-var docEl = document.documentElement,
-resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-recalc = function () {
-    var clientWidth = docEl.clientWidth || 375;
-    clientWidth > 750 ? clientWidth = 750 : clientWidth = clientWidth;
-    if (clientWidth){
-        const fz = docEl.style.fontSize = 20 * (clientWidth / 375);
-        docEl.style.fontSize = 20 * (clientWidth / 375) + 'px';
-        window.remscale = clientWidth / 375;
-        var realfz = ~~(+window.getComputedStyle(document.getElementsByTagName("html")[0]).fontSize.replace('px','')*10000)/10000
-        if (fz !== realfz) {
-            document.getElementsByTagName("html")[0].style.cssText = 'font-size: ' + fz * (fz / realfz) +"px";
-        }
-    }
-};
-if (document.addEventListener){
-window.addEventListener(resizeEvt, recalc, false);
-document.addEventListener('DOMContentLoaded', recalc, false);
 }
 
+$('#browser').text(`font size -> ${$('html').css('font-size')}`)
 
