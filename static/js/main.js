b13b11a27c772941cb59b765ce6241dee0aed2c7
ivotrifonov <ivko999@abv.bg>
2021-02-05 17:54:58 +0200

    test

--- static/js/main.js
+++ static/js/main.js
@@ -21,9 +21,7 @@ $(".arrow-up").on('click', function () {
 
 
 if (isFacebookOrInsta()) {
-  let { width } = screen;
-  // let pixels = width < 361 ? 13 : width < 480 ? 14 : width < 980 ? 15 : 16;
-  let pixels = width < 980 ? 15 : 16;
+  let pixels = 16;
   
   $('html').css({
     'font-size': `${pixels}px`
