e88eee5cae2f2f658e2346697827ed9914542ead
ivotrifonov <ivko999@abv.bg>
2021-02-14 11:33:54 +0200

    test

--- config/express.js
+++ config/express.js
@@ -18,14 +18,15 @@ module.exports = (app) => {
   app.use(express.static(path.join(__dirname + '/../static')));
   app.use(cookieParser());
 
-  // if (process.env.NODE_ENV === 'production') {
-  //   app.use((req, res, next) => {
-  //     if (req.header('x-forwarded-proto') !== 'https')
-  //       res.redirect(`https://${req.header('host')}${req.url}`)
-  //     else
-  //       next()
-  //   })
-  // }
+  if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https') {
+        res.redirect(`https://${req.header('host')}${req.url}`);
+      } else {
+        next();
+      }
+    })
+  }
 
   app.use(compression());
   app.use(bodyParser.urlencoded({
